32771685bb9688509a387849cfc9f3b21f381c62
ujwala2906 <ujwala.dhanule@successive.tech>
2020-03-23 11:14:51 +0530

    UI- Left Side bar component

---
+++ src/component/feature/styleTreeItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TreeItem from "@material-ui/lab/TreeItem";
+import Typography from "@material-ui/core/Typography";
+
+import useTreeItemStyles from "../style";
+
+const StyleTreeItem = (props) => {
+    const classes = useTreeItemStyles();
+    const { labelText, labelIcon: LabelIcon, ...other } = props;
+    return (
+        <>
+            <TreeItem
+                label={
+                    <div className={classes.labelRoot}>
+                        <LabelIcon color="inherit" className={classes.labelIcon} />
+                        <Typography variant="subtitle1" className={classes.labelText}>
+                            {labelText}
+                        </Typography>
+                    </div>
+                }
+                classes={{
+                    root: classes.root,
+                    content: classes.content,
+                    expanded: classes.expanded,
+                    selected: classes.selected,
+                    group: classes.group,
+                    label: classes.label
+                }}
+                {...other}
+            />
+        </>
+    )
+};
+
+export default StyleTreeItem;
---
+++ src/component/sideBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeView from "@material-ui/lab/TreeView";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+
+// features
+import StyleTreeItem from "./feature/styleTreeItem";
+import TreeViewItem from "./feature/treeViewItem";
+
+const SideBar = () => {
+  const useStyles = makeStyles({
+    root: {
+      height: 264,
+      flexGrow: 1,
+      maxWidth: 400
+    }
+  });
+  const classes1 = useStyles();
+
+  return (
+    <>
+      <TreeView
+        className={classes1.root}
+        defaultCollapseIcon={<ArrowDropDownIcon />}
+        defaultExpandIcon={<ArrowRightIcon />}
+      >
+        <StyleTreeItem
+          nodeId="1"
+          labelText="Browse Tools"
+          labelIcon={DashboardIcon}
+        >
+          <TreeViewItem nodeId="2" labelText="All" />
+          <TreeViewItem nodeId="3" labelText="Insight Type">
+            <TreeViewItem nodeId="4" labelText="Audience" />
+            <TreeViewItem nodeId="5" labelText="Business Intelligence" />
+            <TreeViewItem nodeId="6" labelText="Category & Competitive" />
+            <TreeViewItem nodeId="7" labelText="Culture & Trends" />
+            <TreeViewItem nodeId="8" labelText="Media" />
+          </TreeViewItem>
+        </StyleTreeItem>
+        <StyleTreeItem
+          nodeId="9"
+          labelText="Saved Tools"
+          labelIcon={BookmarkIcon}
+        />
+      </TreeView>
+    </>
+  );
+};
+
+export default SideBar;
